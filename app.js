b98bca8f814c05a5b0acd4ec94f00b572f54379a
David F Mueth <davidmueth@gmail.com>
2021-10-03 16:09:27 -0500

    exported template for HTML page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt ([
@@ -130,12 +132,16 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+//         const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+         if(err) throw new Error(err);
+
+        //  console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 // const profileDataArgs = process.argv.slice(2);
